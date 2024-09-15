51ec8da10b34d2c9b8687495835673df61b6e790
hungpham28 <hungpham28092003@gmail.com>
2024-09-15 21:14:06 +0700

    cart show

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const checkSession = require('../middleware/checkSession');
 const IndexController = require('../controllers/Product/IndexController');
-const ResetController = require('../controllers/Product/ResetController');
 
 module.exports = app => {
     const redisClientService = app.get('redisClientService');
